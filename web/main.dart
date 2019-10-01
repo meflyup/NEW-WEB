9bf6ad8e7414d83556f5da967a8726a214c8a51d
zhiweiling <1062206161@qq.com>
2019-10-01 10:13:31 +0800

    第一次作业

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,30 @@
 import 'dart:html';
+import 'dart:math';
+
+
+Element name;
+ButtonElement CallButton;
+List<String> scrabbleLetters = ['1','2','3','4','5','6','7','8','9','10'];
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  name = querySelector("#name");
+
+  CallButton = querySelector("#CallButton");
+  CallButton.onClick.listen(newname);
+
+}
+
+
+void newname(Event e) {
+  name.children.clear();
+  generateNewname();
+}
+
+generateNewname() {
+  Random indexGenerator = new Random();
+  int NameIndex = indexGenerator.nextInt(10);
+  ButtonElement Button = new ButtonElement();
+  Button.classes.add("name");
+  Button.text = scrabbleLetters[NameIndex];
+  name.children.add(Button);
 }
